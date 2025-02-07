8132d1762b6dbf244bc6791090e8589aba2515ad
Naitikj-28 <naitikjain2810@gmail.com>
2025-02-07 12:56:08 +0530

    CSS Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,36 @@ export default function Home() {
     <div className={styles.container}>
       <h1 className={styles.title}>Welcome to the Games Station</h1>
       <ul className={styles.list}>
-        <li className={styles['list-item']}><Link href="/components/tic-tac-toe">Tic-Tac-Toe</Link></li>
-        <li className={styles['list-item']}><Link href="/components/rock-paper-scissors">Rock-Paper-Scissors</Link></li>
-        <li className={styles['list-item']}><Link href="/components/memory-game">Memory Game</Link></li>
-        <li className={styles['list-item']}><Link href="/components/joke-generator">Jokes</Link></li>
-        <li className={styles['list-item']}><Link href="/components/ping-pong">Ping Pong</Link></li>
-        <li className={styles['list-item']}><Link href="/components/snake-game">Snake & Food</Link></li>
+        <li className={styles['list-item']}>
+          <Link href="/components/tic-tac-toe" className={styles['full-link']}>
+            Tic-Tac-Toe
+          </Link>
+        </li>
+        <li className={styles['list-item']}>
+          <Link href="/components/rock-paper-scissors" className={styles['full-link']}>
+            Rock-Paper-Scissors
+          </Link>
+        </li>
+        <li className={styles['list-item']}>
+          <Link href="/components/memory-game" className={styles['full-link']}>
+            Memory Game
+          </Link>
+        </li>
+        <li className={styles['list-item']}>
+          <Link href="/components/joke-generator" className={styles['full-link']}>
+            Jokes
+          </Link>
+        </li>
+        <li className={styles['list-item']}>
+          <Link href="/components/ping-pong" className={styles['full-link']}>
+            Ping Pong
+          </Link>
+        </li>
+        <li className={styles['list-item']}>
+          <Link href="/components/snake-game" className={styles['full-link']}>
+            Snake & Food
+          </Link>
+        </li>
       </ul>
     </div>
   );
